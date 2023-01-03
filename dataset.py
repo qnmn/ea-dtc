9f6ec62bfb701614cbf72d4bd3638c01636b73ad
qnmn <49859929+qnmn@users.noreply.github.com>
2023-01-03 23:09:21 +0100

    Implement evolving dataset

--- dataset.py
+++ dataset.py
@@ -1,16 +1,72 @@
 #!/usr/bin/env python3
 import scipy.io
+import numpy as np
+import random
 
 import decisiontree
+
+random.seed(2)
 
 wine_data = scipy.io.loadmat('./data/wine.mat')
 
 X = wine_data['X']
 y = wine_data['y'].ravel()
 
+np.resize(X, (X.shape[0], X.shape[1] + 1))
+
+X[:,-1] = y
+
 attribute_names = [info[0] for info in wine_data["attributeNames"][0]]
 class_names = [info[0][0] for info in wine_data["classNames"]]
 
-tree = decisiontree.generate_random_tree(None, [0, 1])
+# Shuffle data randomly to evenly distribute the classes among the records.
+X_shuffled = np.zeros(X.shape)
+shuffled_order = list(range(X.shape[0]))
+random.shuffle(shuffled_order)
+for old, new in enumerate(shuffled_order):
+    X_shuffled[new,:] = X[old,:]
 
-print(tree.score(X, y))
+tree = decisiontree.DecisionTree(X_shuffled)
+
+original_tree = tree.copy()
+
+print(f'{X.shape[0]} datapoints')
+
+f2 = tree.score()
+print(f2)
+
+# Target fitness is set to the maximum achieved fitness found before increasing
+# the dataset_size. Thereafter it is lowered by 0.0001 per generation but always
+# 0.9 at minimum.
+target_fitness = 1.0
+
+last_successful = None
+
+for iteration in range(1000):
+    copy = tree.copy()
+    copy.mutate_leaf()
+    f3 = copy.score()
+    if f3 > f2:
+        f2 = f3
+        tree = copy
+    if f2 >= target_fitness:
+        last_successful = tree.copy()
+        print(f'Target fitness {target_fitness:.5f} has been achieved on iteration {iteration}, dataset size has been increased.')
+        print('by tree: with score ', f2, ' over ', tree.dataset_size, ' records')
+        tree.increase_dataset_size()
+        target_fitness = f2
+        f2 = tree.score()
+    else:
+        target_fitness = max(0.9, target_fitness - 1e-4)
+
+
+print('-- results --')
+print(f2)
+print(tree)
+print(tree.score())
+print(tree.dataset_size)
+
+print('--')
+print(last_successful)
+print(last_successful.score())
+print(last_successful.dataset_size)
--- decisiontree.py
+++ decisiontree.py
@@ -4,19 +4,24 @@ import random
 
 from typing import *
 
+import numpy as np
+
 
 class Operator(Enum):
     LT = auto()
     EQ = auto()
     GT = auto()
 
+
 class Node:
 
-    def __init__(self, var, val, is_pred=True, op=Operator.EQ):
+    def __init__(self, tree, var, val, is_pred=True, op=Operator.EQ):
         self.var = var
         self.is_pred = is_pred
         self.op = op
         self.val = val
+
+        self.tree: DecisionTree = tree
 
         self.left: Optional[Node] = None
         self.right: Optional[Node] = None
@@ -31,20 +36,132 @@ class Node:
             else:
                 return self.right.decide(entry)
 
+    def split_leaf(self):
+        self.var = random.choice([
+            var for var in range(self.tree.data.shape[1])
+            if var != self.tree.class_attr
+        ])
+        self.is_pred = False
+
+        self.op = random.choice([Operator.LT,
+                                 Operator.GT])  # TODO: Operator.EQ
+
+        v_min, v_max = self.tree.ranges[:, self.var]
+        self.val = v_min + random.random() * (v_max - v_min)
+
+        # Create two child predictions.
+        self.left = self.tree.random_prediction()
+        self.right = self.tree.random_prediction()
+
+    def mutate_leaf(self, depth_threshold):
+        if depth_threshold <= 0:
+            return False
+        if self.is_pred:
+            # TODO: Other mutations besides splitting.
+            self.split_leaf()
+
+            return True
+        else:
+            # TODO: This choice should be even among all leafs.
+            if random.random() < 0.5:
+                self.left.mutate_leaf(depth_threshold - 1)
+            else:
+                self.right.mutate_leaf(depth_threshold - 1)
+
+    def __repr__(self):
+        return f'{"P" if self.is_pred else "Q"}: {self.var} {self.op} {self.val}'
+
+    def copy(self, new_root):
+        new_node = Node(new_root, self.var, self.val, self.is_pred, self.op)
+        if self.left is not None:
+            new_node.left = self.left.copy(new_root)
+        if self.right is not None:
+            new_node.right = self.right.copy(new_root)
+        return new_node
+
+    def iterate_nodes(self):
+        if self.left is not None:
+            yield from self.left.iterate_nodes()
+        yield self
+        if self.right is not None:
+            yield from self.right.iterate_nodes()
+
+
+def compute_ranges(data):
+    ranges = np.zeros((2, data.shape[1]))
+    ranges[0, :] = np.min(data, axis=0)
+    ranges[1, :] = np.max(data, axis=0)
+    return ranges
+
+
 class DecisionTree:
-    def __init__(self, root=None):
+
+    def __init__(self,
+                 data,
+                 dataset_size=0,
+                 class_attribute=None,
+                 root=None,
+                 max_depth=20,
+                 ranges=None):
+        self.data = data
+        if dataset_size <= 0:
+            self.dataset_size = 2
+        else:
+            self.dataset_size = dataset_size
+        self.max_depth = max_depth
+        if class_attribute is None:
+            self.class_attr = data.shape[1] - 1
+        else:
+            self.class_attr = class_attribute
         self.root: Optional[Node] = root
+        if ranges is None:
+            self.ranges = compute_ranges(self.data)
+        else:
+            self.ranges = ranges
 
-    def score(self, x_test, y_test):
+        if root is None:
+            predicted_class = random.choice(
+                range(int(self.ranges[1, self.class_attr]) + 1))
+            self.root = self.random_prediction()
+
+    def copy(self):
+        tree = DecisionTree(self.data,
+                            self.dataset_size,
+                            self.class_attr,
+                            root=None,
+                            max_depth=self.max_depth,
+                            ranges=self.ranges)
+        tree.root = self.root.copy(tree)
+        return tree
+
+    def score(self, test_data=None):
         if self.root is None:
-            return None
+            return 0
+        if test_data is None:
+            test_data = self.data
+            num_entries = self.dataset_size
+        else:
+            num_entries = test_data.shape[0]
 
-        return sum(self.decide(x_test[i,:]) == y_test[i] for i in range(x_test.shape[0]))
+        return sum(
+            self.decide(test_data[i, :]) == test_data[i, self.class_attr]
+            for i in range(num_entries)) / num_entries
 
     def decide(self, entry):
         return self.root.decide(entry)
 
+    def mutate_leaf(self):
+        return self.root.mutate_leaf(self.max_depth)
 
-def generate_random_tree(class_var, classes):
-    predicted_class = random.choice(classes)
-    return DecisionTree(Node(class_var, predicted_class))
+    def random_prediction(self):
+        predicted_class = random.choice(
+            range(int(self.ranges[1, self.class_attr]) + 1))
+        return Node(self, self.class_attr, predicted_class)
+
+    def __repr__(self):
+        return '\n'.join(n.__repr__() for n in self.root.iterate_nodes())
+
+    def increase_dataset_size(self):
+        increase_by = int(1 + 0.02 * self.dataset_size)
+        self.dataset_size = min(self.data.shape[0],
+                                self.dataset_size + increase_by)
